4d183eb43994585447ed6142bf5f49f367bcd7bf
Kevin Lacker <lacker@gmail.com>
2020-11-16 15:22:19 -0800

    some basic file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()> {
+    let mut file = File::open("foo.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    println!("contents are {}", contents);
+    Ok(())
 }
