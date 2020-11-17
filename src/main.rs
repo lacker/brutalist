18b60ec8c8368636b93e97d6635ef3fb8d7832f1
Kevin Lacker <lacker@gmail.com>
2020-11-17 11:06:21 -0800

    making a tree

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,19 @@ use regex::Regex;
 use std::fs::File;
 use std::io::prelude::*;
 
+#[derive(Debug)]
+struct Sexp<'a> {
+    token: String,
+    children: Vec<&'a Sexp<'a>>,
+}
+
+fn make_atom(text: &str) -> Sexp {
+    Sexp {
+        token: text.to_string(),
+        children: Vec::new(),
+    }
+}
+
 fn tokenize(text: &str) -> Vec<&str> {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"([A-Za-z_]+|\S)").unwrap();
@@ -17,5 +30,8 @@ fn main() -> std::io::Result<()> {
     println!("contents are:\n {}", contents);
     let tokens = tokenize(&contents);
     println!("tokens are:\n {:?}", tokens);
+
+    println!("XXX {:?}", make_atom("foo"));
+
     Ok(())
 }
