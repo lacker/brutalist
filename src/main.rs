e496892e8a4bbcc614ae09e36f1a4805c0163785
Kevin Lacker <lacker@gmail.com>
2020-11-18 09:10:23 -0800

    move stuff around a bit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use lazy_static::lazy_static;
 use regex::Regex;
 use std::collections::VecDeque;
+use std::fmt;
 use std::fs::File;
 use std::io::prelude::*;
 
-#[derive(Debug)]
 enum Sexp {
     Atom(String),
     List(Vec<Sexp>),
@@ -15,6 +15,19 @@ impl Sexp {
         match self {
             Sexp::Atom(s2) => s1 == s2,
             Sexp::List(_) => false,
+        }
+    }
+}
+
+impl fmt::Display for Sexp {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Sexp::Atom(s) => write!(f, "{}", s),
+            Sexp::List(v) => {
+                let subs = v.iter().map(|s| s.to_string());
+                let joined = subs.collect::<Vec<_>>().join(" ");
+                write!(f, "({})", joined)
+            }
         }
     }
 }
@@ -51,13 +64,13 @@ fn deparenthesize(tokens: Vec<&str>) -> Sexp {
 
 fn tokenize(text: &str) -> Vec<&str> {
     lazy_static! {
-        static ref RE: Regex = Regex::new(r"([A-Za-z_]+|\S)").unwrap();
+        static ref RE: Regex = Regex::new(r"([A-Za-z_]+|<=|\S)").unwrap();
     }
     RE.find_iter(text).map(|m| m.as_str()).collect()
 }
 
 fn main() -> std::io::Result<()> {
-    let mut file = File::open("fne/BOO109+1.p")?;
+    let mut file = File::open("tptp/FNE/BOO109+1.p")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     println!("contents are:\n {}", contents);
@@ -65,7 +78,7 @@ fn main() -> std::io::Result<()> {
     println!("tokens are:\n {:?}", tokens);
 
     let s = deparenthesize(tokens);
-    println!("sexp is: {:?}", s);
+    println!("sexp is: {}", s);
 
     Ok(())
 }
