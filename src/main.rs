198bd28cc3e2293c09b3794b3825df14803908fe
Kevin Lacker <lacker@gmail.com>
2020-11-19 14:07:05 -0800

    Fix formatting

--- src/main.rs
+++ src/main.rs
@@ -198,8 +198,13 @@ impl fmt::Display for Formula {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Formula::Atomic(t) => write!(f, "{}", t),
-            Formula::And(left, right) => write!(f, "({} & {})", left, right),
-            _ => write!(f, "XXX"),
+            Formula::And(left, right) => write!(f, "({} and {})", left, right),
+            Formula::Or(left, right) => write!(f, "({} or {})", left, right),
+            Formula::Not(arg) => write!(f, "not {}", arg),
+            Formula::Implies(left, right) => write!(f, "({} => {})", left, right),
+            Formula::Iff(left, right) => write!(f, "({} iff {})", left, right),
+            Formula::ForAll(v, exp) => write!(f, "∀{} {}", v, exp),
+            Formula::Exists(v, exp) => write!(f, "∃{} {}", v, exp),
         }
     }
 }
