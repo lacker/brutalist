ac8a22bed1179871d9603ede629dc50e44b0ea78
Kevin Lacker <lacker@gmail.com>
2020-12-10 11:04:59 -0800

    Test sub validity

--- src/cnf.rs
+++ src/cnf.rs
@@ -1,4 +1,7 @@
 #![allow(dead_code)]
+use std::collections::HashMap;
+use std::fmt;
+
 // This file describes how to represent statements in clausal normal form (CNF).
 // These data structures are designed for efficiency, using integer ids instead of
 // strings to specify constants, variables, and functions.
@@ -20,6 +23,31 @@ impl Term {
             Term::Function(_, terms) => terms.iter().map(|t| t.weight()).sum::<u32>() + 1,
         }
     }
+
+    pub fn contains_variable(&self, v: u32) -> bool {
+        match self {
+            Term::Constant(_) => false,
+            Term::Variable(v2) => v == *v2,
+            Term::Function(_, terms) => terms.iter().any(|t| t.contains_variable(v)),
+        }
+    }
+}
+
+impl fmt::Display for Term {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Term::Constant(c) => write!(f, "c{}", c),
+            Term::Variable(v) => write!(f, "v{}", v),
+            Term::Function(func, terms) => {
+                let sub = terms
+                    .iter()
+                    .map(|t| t.to_string())
+                    .collect::<Vec<_>>()
+                    .join(", ");
+                write!(f, "f{}({})", func, sub)
+            }
+        }
+    }
 }
 
 #[derive(Clone)]
@@ -38,3 +66,32 @@ impl Literal {
 }
 
 pub type Clause = Vec<Literal>;
+
+// A Substitution maps variables to terms.
+// A valid substitution must not have any terms that contain variables for which it also
+// has keys. In other words, applying the substitution should completely eliminate the variables
+// it is substituting out, not create new ones.
+pub struct Substitution {
+    pub map: HashMap<u32, Term>,
+}
+
+impl Substitution {
+    pub fn new() -> Substitution {
+        Substitution {
+            map: HashMap::new(),
+        }
+    }
+
+    pub fn assert_valid(&self) {
+        for (&var1, term1) in &self.map {
+            for (&var2, term2) in &self.map {
+                if term1.contains_variable(var2) {
+                    panic!(
+                        "inconsistent subs: v{} -> {} and v{} -> {}",
+                        var1, term1, var2, term2
+                    );
+                }
+            }
+        }
+    }
+}
