6f4e2a55ef718b2870dfa0ce33ac7463a6c09ac2
Kevin Lacker <lacker@gmail.com>
2020-12-17 13:12:09 -0800

    prove

--- src/prover.rs
+++ src/prover.rs
@@ -24,8 +24,36 @@ impl Prover {
     }
 
     pub fn insert(&mut self, c: Clause) {
+        if self.seen.contains(&c) {
+            return;
+        }
         self.seen.insert(c.clone());
         self.passive.push(Reverse(c));
+    }
+
+    pub fn prove(&mut self) -> bool {
+        loop {
+            if let Some(Reverse(c)) = self.passive.pop() {
+                if c.literals.len() == 0 {
+                    return true;
+                }
+
+                for new_clause in c.factor().into_iter() {
+                    self.insert(new_clause);
+                }
+
+                let mut new_clauses = Vec::new();
+                for clause in &self.active {
+                    new_clauses.extend(c.resolve(&clause).into_iter());
+                }
+                for new_clause in new_clauses {
+                    self.insert(new_clause);
+                }
+            } else {
+                // We ran out of ways to continue the search
+                return false;
+            }
+        }
     }
 }
 
@@ -37,5 +65,6 @@ mod tests {
     fn test_prove() {
         let mut p = Prover::new();
         p.insert(Clause::read("(f1 X1) (-(f2 X1))"));
+        p.insert(Clause::read("(f2 k1) k2"));
     }
 }
